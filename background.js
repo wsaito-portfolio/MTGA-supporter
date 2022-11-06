089dc735fdb8e2dc3350d939a773bec7fe3db1a9
wsaito-portfolio <65600797+wsaito-portfolio@users.noreply.github.com>
2022-11-06 23:07:11 +0900

    fix async processsing
    非同期処理できるように修正

--- background.js
+++ background.js
@@ -4,66 +4,98 @@
 chrome.runtime.onInstalled.addListener(() => {
   //カードリスト読み込み
   fetch(chrome.runtime.getURL("/json/DMU_list.json"))
-  .then((response) => response.json())  .then((data) => {
+    .then((response) => response.json()).then((data) => {
 
-    var key,value;
-    console.log(data);
-    data.forEach(element => {
-      key = element["col_num"]+element["set"];
-      value = element["name_ja"];
-      chrome.storage.local.set( { [key] : [value]});
-      
-      element["col_num"]+element["col_num"]
+      var key, value;
+      //console.log(data);
+      //Chromeのローカルに保存
+      data.forEach(element => {
+        key = element["col_num"] + element["set"];
+        value = element["name_ja"];
+        chrome.storage.local.set({ [key]: [value] });
+
+        element["col_num"] + element["col_num"]
+      });
+
     });
-    
-  });
 
-  
+
   var parent = chrome.contextMenus.create({
     id: 'parent',
     title: 'MTGA-Supporter'
   });
 });
 
-chrome.contextMenus.onClicked.addListener(function(info,tab) {
-  chrome.tabs.sendMessage(tab.id, 
+chrome.contextMenus.onClicked.addListener(function (info, tab) {
+  chrome.tabs.sendMessage(tab.id,
     {
-        message: "copyText",
-        textToCopy: "some text" 
-    }, (response) => {  
+      message: "copyText",
+      textToCopy: "some text"
+    }).then( async(response) => {
+
+      //デッキリスト日本語化処理開始
       let text;
+      //一行ごとに区切って配列化する
       text = String(response).split('\r\n');
-      
-      if (String(text[1]).match(/[0-9]{1,2}.*\([a-zA-Z]{3}\)\ [0-9]{1,3}/).length>0){
-      
-        text = String(response).split('\r\n');  
-        
 
-        //カードの枚数の切り出し
-        var text1 = String(text[1]).match(/[0-9]{1,3}/);
-        text[1] =text[1].replace(/[0-9]{1,3}/,"");
+      //console.log(text);
 
-        //カードNoの切り出し
-        var text2 = String(text[1]).match((/[0-9]{1,3}/));
-        text[1] =text[1].replace(/([0-9]{1,3})/,"");
+      let num;  //カード枚数
+      let col_num; //コレクションナンバー
+      let set;  //収録セット
+      let id
+      let text_ja;
 
-        //カードセットの切り出し
-        var text3 = String(text[1]).match(/(\([a-zA-Z]{3}\))/);
-        text[1] =text[1].replace(/(\([a-zA-Z]{3}\))/,"");
+      //デッキインポートの形式チェック
+      if (String(text[1]).match(/[0-9]{1,2}.*\([a-zA-Z]{3}\)\ [0-9]{1,3}/).length > 0) {
+
+
         //
-        console.log(text1[0]);
-        console.log(text2[0]);
-        console.log(text3[0]);
-        console.log(text[1]);
+        for(let line of text) {
+          if (line.match(/[0-9]{1,2}.*\([a-zA-Z]{3}\)\ [0-9]{1,3}/) == null) continue;
 
-      }else if(String(text[1]).match(/[0-9]{1,2}.*/).length>0){
+          //console.log("カードの枚数の切り出し");
+          //カードの枚数の切り出し
+          line = String(line);
+
+          num = String(line).slice(0, 2);
+          line = line.slice(2);
+
+          //console.log("カードNoの切り出し");
+          //カードNoの切り出し
+          col_num = String(line).match((/[0-9]{1,3}/));
+          line = line.replace(/([0-9]{1,3})/, "");
+
+          //console.log("カードセットの切り出し");
+          //カードセットの切り出し
+          set = String(line).match(/(\([a-zA-Z]{3}\))/g);
+          line = line.replace(/(\([a-zA-Z]{3}\))/g, "");
+
+          //console.log(col_num);
+          //console.log(set);
+          console.log(col_num+set);
+          id = String(col_num + set)
+          //カードのマッチング
+          await chrome.storage.local.get([id])
+            .then((name_ja) => {
+
+              //console.log("日本語翻訳中");
+              text_ja = num + " " + name_ja[String(col_num + set)] + " " + set + " " + col_num + '\r\n';
+              console.log(text_ja);
+              //console.log("日本語翻訳終了");
+            });
+        }
 
 
-  
-        console.log("test");
+
+
+      } else if (String(text[1]).match(/[0-9]{1,2}.*/).length > 0) {
+
+
+
+        //console.log("test");
 
       }
-      
-      console.log(response);
+
     })
 });
